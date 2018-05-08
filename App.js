a20ee6899c49d81714a76f281c494d69c8b69896
Nedim Becirovic <nedimb86@gmail.com>
2018-05-08 06:22:24 +0200

    Initial Commit

--- App.js
+++ App.js
@@ -1,23 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+export default class FlexDimensionsBasics extends Component {
+    render() {
+        return (
+            <View style={{flex: 1}}>
+                <View style={{flex: 3, backgroundColor: 'powderblue'}} >
+                    <View style={{width: 50, height: 50, backgroundColor: 'orange'}} />
+                    <View style={{width: '50%', height: 50, backgroundColor: 'green'}} />
+                    <View style={{width: 150, height: 50, backgroundColor: 'red'}} />
+                </View>
+                <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+                <View style={{flex: 1, backgroundColor: 'steelblue'}} />
+            </View>
+        );
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
